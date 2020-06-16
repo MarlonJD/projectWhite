c4fb64b297456405579293a4381fb8a5cb862b96
Burak Karahan <burak.karahan@mail.ru>
2020-06-16 17:09:24 +0300

    Add permissions

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 # from django.shortcuts import render
 from django.views.generic import (ListView, TemplateView)
-# from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 # from django.contrib.auth.decorators import login_required
 # from django.urls import reverse_lazy
 from .models import Stock
@@ -9,16 +9,16 @@ from .models import Stock
 # import json
 
 
-class StockListView(ListView):
+class StockListView(LoginRequiredMixin, ListView):
     "Main Home Page"
     model = Stock
     paginate_by = 20
     template_name = 'main/index.html'
 
 
-class StockChangePage(TemplateView):
+class StockChangePage(LoginRequiredMixin, TemplateView):
     template_name = 'main/stock_change.html'
 
 
-class ProductPage(TemplateView):
+class ProductPage(LoginRequiredMixin, TemplateView):
     template_name = 'main/product.html'
