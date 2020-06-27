0f8b080e672303c567278422cf9216657491cc1e
Burak Karahan <burak.karahan@mail.ru>
2020-06-27 19:26:38 +0300

    Add token auth

--- api/urls.py
+++ api/urls.py
@@ -1,16 +1,20 @@
 from rest_framework import routers
 from django.urls import path, include
+from rest_framework.authtoken import views as rest_views
 from . import views
 
 router = routers.DefaultRouter()
 router.register(r'stock', views.StockViewSet, basename='stocks')
 router.register(r'product', views.ProductViewSet, basename='products')
-router.register(r'stockFromProduct', views.StockFromProductViewSet,
+router.register(r'stockFromProduct',
+                views.StockFromProductViewSet,
                 basename='stock_from_product')
-
 
 urlpatterns = [
     path('', include(router.urls)),
     path('loadProduct/<key>/', views.loadProductsAPIView.as_view()),
-    path('searchProduct/', views.searchProduct.as_view())
+    path('searchProduct/', views.searchProduct.as_view()),
+    path('api-token-auth/',
+         rest_views.obtain_auth_token,
+         name='api-token-auth'),
 ]
