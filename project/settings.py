ce94efb8f0fde0b963c4d7b0e554403c7e8ee31e
Burak Karahan <burak.karahan@mail.ru>
2020-08-23 17:54:33 +0300

    Add db pass

--- project/settings.py
+++ project/settings.py
@@ -105,7 +105,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
 
